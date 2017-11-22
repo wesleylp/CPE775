8dbf664d1ac70971ed631fbac25dfced31da4a1d
Wesley Lobato Passos <wesley.passos@moscou.smt.ufrj.br>
2017-11-22 10:35:38 -0200

    testing

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib
+import glob
+from sklearn import cluster
+
+a = np.array((2,3))
+print(a)

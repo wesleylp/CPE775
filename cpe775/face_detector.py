ad0daa4f7a290886bf2c845efc89ee5f777a961e
Igor Macedo Quintanilha <igor.quintanilha@poli.ufrj.br>
2018-04-19 09:44:16 -0300

    Add crop Add load dict if model is not none

--- cpe775/face_detector.py
+++ cpe775/face_detector.py
@@ -16,19 +16,30 @@ class Base(object):
         self.cuda = cuda
         self.size = size
 
-        self.net.load_state_dict(torch.load(self.model)['state_dict'])
+        if self.model is not None:
+            self.net.load_state_dict(torch.load(self.model)['state_dict'])
         self.net.eval()
 
         if self.cuda:
             self.net.cuda()
 
-    def __call__(self, x, bgr=True):
+    def __call__(self, x, bgr=True, crop=None):
 
         if bgr and x.shape[-1] == 3:
             x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
 
         x = cv2.resize(x, (self.size, self.size),
                        interpolation = cv2.INTER_CUBIC)
+
+        if crop is not None:
+            h, w = x.shape[:2]
+
+            th, tw = crop, crop
+
+            i = int(round((h - th) / 2.))
+            j = int(round((w - tw) / 2.))
+
+            x = x[i:i+th, j:j+tw, ...]
 
         ndims = 4
         if len(x.shape) == 2:
@@ -46,6 +57,9 @@ class Base(object):
 
         x_var = torch.autograd.Variable(torch.from_numpy(x).float(),                                            volatile=True)
 
+        if self.cuda:
+            x_var = x_var.cuda()
+
         y = self.net(x_var).data.cpu().numpy()
 
         if ndims != 4:
@@ -55,30 +69,53 @@ class Base(object):
 
 class LandmarksDetector(Base):
 
-    def __init__(self, net, model, size=224, cuda=False):
+    def __init__(self, net, model, size=256, cuda=False):
+        self.orig_size = size
         super(LandmarksDetector, self).__init__(net, model, size, cuda)
 
 
-    def from_rect(self, image, rect, enlarge_ratio = 1.7, bgr=True, to_gray=True):
+    def from_rect(self, image, rect, enlarge_ratio = 1.7, bgr=True, to_gray=True, crop=224):
         rect = utils.rect_to_tuple(rect)
         bbox = utils.get_bbox(*rect)
 
+
         large_bbox = utils.enlarge_bbox(bbox, enlarge_ratio)
 
-        increased_img, large_bbox, _, _ = utils.increase_img_size(image, large_bbox)
+        increased_img, large_bbox, x_extra_start, y_extra_start = utils.increase_img_size(image, large_bbox)
 
         left, top, w, h = large_bbox
 
         cropped_image = increased_img[top:top + h, left:left + w, :]
+     
+        landmarks = self(cropped_image, bgr, to_gray, crop)
 
-        return self(cropped_image, bgr, to_gray) * [w, h] + [left, top], rect
+        if crop is not None:
+            # Adjusting the center cropping
+            th, tw = crop, crop
+            i = int(round((self.size - th) / 2.))
+            j = int(round((self.size - tw) / 2.))
 
-    def __call__(self, x, bgr=True, to_gray=True):
+            landmarks *= [tw, th]
+            landmarks += [j, i]
+        else:
+            landmarks *= [self.size, self.size]
+
+        landmarks *= [w/self.size, h/self.size]
+        landmarks -= [x_extra_start, y_extra_start]
+
+        landmarks += [left, top]
+
+
+
+
+        return landmarks, rect
+
+    def __call__(self, x, bgr=True, to_gray=True, crop=None):
         if to_gray:
             cvtColor = cv2.COLOR_BGR2GRAY if bgr else cv2.COLOR_RGB2GRAY
             x = cv2.cvtColor(x, cvtColor)
 
-        y = super(LandmarksDetector, self).__call__(x, bgr)
+        y = super(LandmarksDetector, self).__call__(x, bgr, crop)
 
         return y
 
